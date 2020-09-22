d273aabc0704b0177c41bd41040e9c9ab5619cce
imogenkraak <imogen.kraak@gmail.com>
2020-09-22 12:07:01 +0100

    Added time posted to all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+
   def create
     @post = Post.create(post_params)
     redirect_to posts_url
   end
+
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -15,7 +17,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:message, :created_at)
   end
 
 end
