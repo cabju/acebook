6e0860ccd5bcc7144eb4302dfb580f7af773917b
Tamara Jeffrey <just-tam@live.com>
2020-09-22 15:32:22 +0100

    added ability to sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/user' => "posts#index", :as => :user_root
+  #get 'sign_in'  => 'users#new' 
+  
+  resources :posts, :users
 
-  resources :posts
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
