17145bfc63a582b863ee799e25f7b6b690918dc8
Tamara Jeffrey <just-tam@live.com>
2020-09-23 17:55:19 +0100

    adding dependency for users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :posts
+  has_many :posts, dependent: :destroy
   #has_many :comments, through posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
