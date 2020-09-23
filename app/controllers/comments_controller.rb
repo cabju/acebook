5a1ac3554f73a25e3889c003af804f36d863493c
Tamara Jeffrey <just-tam@live.com>
2020-09-23 14:12:18 +0100

    users can post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
 
         @post = Post.find(params[:post_id])
          #@comment = @post.comments.create(params[:comment].permit(:name, :comment))
-         @comment = post.comments.create(comment_params.merge(user_id: current_user.id))
+         @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
         
         redirect_to post_path(@post) 
 
@@ -43,7 +43,7 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
-        params.require(:comment).permit(:comment, :created_at, :post_id)
+        params.require(:comment).permit(:name, :comment, :created_at, :post_id)
     end
 
 end
