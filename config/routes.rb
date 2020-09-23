5630fd07ade8a8d741991f23b8c3590f9cf19b28
Tamara Jeffrey <just-tam@live.com>
2020-09-23 10:29:58 +0100

    added comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+resources :posts do
+  resources :comments
+end
   resources :posts
 end
