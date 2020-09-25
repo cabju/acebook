f25fad59a0daada974c57cc070643d16a71b35fc
Tamara Jeffrey <just-tam@live.com>
2020-09-25 17:49:03 +0100

    styling home and posts pages

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
-  let(:post) { Post.new(user_id: 1) }
+  #let(:post) { Post.new(user_id: 1) }
   describe "GET /posts/new " do
     it "responds with 200" do
     
@@ -13,6 +13,8 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
+      User.create(email: "test@email.com", password: "password")
+      sign_in User.first
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
